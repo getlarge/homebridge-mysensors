5f149d05ff1c8378b8d0919d0ca519d97d73aef2
getlarge <ed@getlarge.eu>
2022-01-16 16:46:08 +0100

    feat: create MySensorsMqttTransport class

--- src/transport.ts
+++ src/transport.ts
@@ -1,9 +1,23 @@
 import EventEmitter from 'events';
-import { MqttClient } from 'mqtt';
+import { readFileSync } from 'fs';
+import { Logger } from 'homebridge';
+import {
+  connect,
+  IClientOptions,
+  IClientPublishOptions,
+  MqttClient,
+} from 'mqtt';
 import SerialPort from 'serialport';
 
-import { PluginConfiguration } from './configModels';
-import { Commands, MySensorsProtocol, Transport } from './mySensors/protocol';
+import { MqttConfiguration, PluginConfiguration } from './configModels';
+import { mySensorsProtocolDecoder } from './converter/decoder';
+import { errorToString } from './helpers';
+import {
+  Commands,
+  MySensorsMqttPattern,
+  MySensorsProtocol,
+  Transport,
+} from './mySensors/protocol';
 
 export interface MySensorsTransportEvents {
   presentation: (
@@ -91,4 +105,141 @@ export abstract class MySensorsTransport<
   abstract publishMessage(...args: any): void | Promise<void>;
 
   abstract isConnected(): boolean;
+}
+
+export class MySensorsMqttTransport extends MySensorsTransport<MqttClient> {
+  private readonly mqttConfig: Partial<MqttConfiguration>;
+
+  constructor(
+    public readonly log: Logger,
+    readonly config: PluginConfiguration
+  ) {
+    super(config, Transport.MQTT);
+    this.mqttConfig = this.config.mqtt || {};
+    this.onMessage = this.onMessage.bind(this);
+    this.client = this.initializeClient(config);
+  }
+
+  private createOptions(): IClientOptions {
+    const {
+      version,
+      keepalive,
+      ca,
+      key,
+      cert,
+      user,
+      password,
+      rejectUnauthorized,
+      clientId,
+    } = this.mqttConfig;
+    const options: IClientOptions = {};
+    if (version) {
+      options.protocolVersion = version;
+    }
+
+    if (keepalive) {
+      this.log.debug(`Using MQTT keepalive: ${keepalive}`);
+      options.keepalive = keepalive;
+    }
+
+    if (ca) {
+      this.log.debug(`MQTT SSL/TLS: Path to CA certificate = ${ca}`);
+      options.ca = readFileSync(ca);
+    }
+
+    if (key && cert) {
+      this.log.debug(`MQTT SSL/TLS: Path to client key = ${key}`);
+      this.log.debug(`MQTT SSL/TLS: Path to client certificate = ${cert}`);
+      options.key = readFileSync(key);
+      options.cert = readFileSync(cert);
+    }
+
+    if (user && password) {
+      options.username = user;
+      options.password = password;
+    }
+
+    if (clientId) {
+      this.log.debug(`Using client ID: '${clientId}'`);
+      options.clientId = clientId;
+    }
+
+    if (rejectUnauthorized !== undefined && !rejectUnauthorized) {
+      this.log.debug(
+        'MQTT reject_unauthorized set false, ignoring certificate warnings.'
+      );
+      options.rejectUnauthorized = false;
+    }
+
+    return options;
+  }
+
+  initializeClient(config: PluginConfiguration): MqttClient | undefined {
+    if (!config.mqtt?.server) {
+      this.log.error('No MQTT server defined');
+      return;
+    }
+    this.log.info(`Connecting to MQTT server at ${config.mqtt.server}`);
+    const options = this.createOptions();
+    const mqttClient = connect(config.mqtt.server, options);
+    mqttClient.on('connect', () => {
+      this.log.info('Connected to MQTT server');
+    });
+    return mqttClient;
+  }
+
+  openListener(): void {
+    // Setup MQTT callbacks and subscription
+    if (this.client instanceof MqttClient) {
+      this.client.on('message', this.onMessage);
+      // TODO: limit subscriptions ? for specific gateways ?
+      this.client.subscribe('#');
+    }
+  }
+
+  onMessage(topic: MySensorsMqttPattern, payload: Buffer): void {
+    const protocol = mySensorsProtocolDecoder(
+      Transport.MQTT,
+      topic,
+      payload.toString()
+    );
+    if (!protocol) {
+      this.log.debug(
+        'Ignore message, because protocol does not match MySensors MQTT API.',
+        topic
+      );
+      return;
+    }
+    super.handleMessage(protocol, Transport.MQTT);
+  }
+
+  publishMessage(
+    topic: MySensorsMqttPattern,
+    payload: string,
+    options: IClientPublishOptions = {}
+  ): Promise<void> {
+    if (this.config !== undefined) {
+      options = { qos: 0, retain: false, ...options };
+      if (!this.isConnected) {
+        this.log.error('Not connected to MQTT server!');
+        this.log.error(`Cannot send message '${topic}': '${payload}`);
+        return Promise.resolve();
+      }
+
+      this.log.info(`Publish '${topic}': '${payload}'`);
+      return new Promise<void>((resolve) => {
+        this.client?.publish(topic, payload, options, (error) => {
+          if (error) {
+            this.log.error(errorToString(error));
+          }
+          resolve();
+        });
+      });
+    }
+    return Promise.resolve();
+  }
+
+  isConnected(): boolean {
+    return this.client !== undefined && !this.client.reconnecting;
+  }
 }
